013013e58f7e78aa90be65dae477c410b99db498
Fidel <carapez31@gmail.com>
2025-05-23 17:40:27 -0600

    first

---
+++ src/main/java/com/mycompany/unogame/Main.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.unogame;
+
+/**
+ *
+ * @author mclovin
+ */
+public class Main {
+     public static void main(String[] args) {
+        // Define los jugadores
+        String[] jugadores = {"Jugador1", "Jugador2", "Jugador3", "Jugador4"};
+
+        // Crea el juego con esos jugadores
+        Juego juego = new Juego(jugadores);
+
+        // Inicia el juego (elige carta válida inicial y aplica efectos si corresponde)
+        juego.start(juego);
+
+        // Imprime el jugador que empieza
+        System.out.println("El juego ha comenzado.");
+        System.out.println("Turno actual: " + juego.getCurrentPlayer());
+
+        // Muestra la carta en la cima del montón
+        System.out.println("Carta actual: " + juego.getTopCard());
+    }
+}
---
+++ src/main/java/com/mycompany/unogame/UnoCard.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.unogame;
+
+/**
+ *
+ * @author mclovin
+ */
+
+public class UnoCard {
+    enum Color
+    {
+        Red, Green, Blue, Yellow, Wild;
+        private static final Color[] colors = Color.values();
+        public static Color getColor(int i)
+        {
+            return Color.colors[i];
+        }
+
+    }
+
+    enum Value
+    {
+        Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine, DrawTwo, Skip, Reverse, Wild, Wild_Four;
+
+        private static final Value[] values = Value.values();
+        public static Value getValue(int i)
+        {
+            return Value.values[i];
+        }
+
+    }
+
+    private final Color color;
+    private final Value value;
+
+    public UnoCard(final Color color, final Value value){
+        this.color = color;
+        this.value = value;
+    }
+
+    public Color getColor() {
+        return this.color;
+    }
+
+    public Value getValue() {
+        return this.value;
+    }
+
+    @Override
+    public String toString(){
+        return color + "__" + value;
+    }
+}
